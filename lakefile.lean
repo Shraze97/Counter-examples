cf1d7e57db76e0f9357c7a07baa263f4b4aec124
Suryansh Shrivastava <86036271+Shraze97@users.noreply.github.com>
2023-11-28 03:52:01 +0530

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -2,6 +2,7 @@ import Lake
 open Lake DSL
 
 package «my_project» {
+  precompileModules := true
   -- add any package configuration options here
 }
 
@@ -9,6 +10,11 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «MyProject» {
+lean_lib «CounterExamples» {
   -- add any library configuration options here
 }
+
+
+
+meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
